4954e9fbe76bc18c2bbe3e1c9340e81a37736876
MELO\v-ing <emanuel.melo@leadstation.com.br>
2022-07-08 22:05:26 -0300

    create global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
+
+import "./GlobalStyles/index.css"
 import { Header } from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
    <Header/>
-   </>
   )
 }
 
